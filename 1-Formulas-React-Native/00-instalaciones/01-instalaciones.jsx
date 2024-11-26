9854b63d75d41c8c657be253a685fe52429e74e7
quintero git <hello.quinteroo@gmail.com>
2024-11-26 22:01:53 +0100

    CREATE 2.1-bases

---
+++ 1-Formulas-React-Native/00-instalaciones/01-instalaciones.jsx
@@ -0,0 +1,7 @@
+
+
+
+//! Instalaciones
+// links con los enlaces
+//? https://gist.github.com/Klerith/f2eea5447f2dc7e90b81ec639a975f96
+
---
+++ 2-Notas/2.1-bases/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1>VITE + REACT + TS</h1>
+    </>
+  )
+}
+
+export default App
