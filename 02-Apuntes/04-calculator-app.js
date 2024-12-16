e0a0c40617c99519e73cb0ffb25c94e1ec7c98fa
qugodev <146204443+Quinteroo@users.noreply.github.com>
2024-12-16 18:33:04 +0100

    Update 04-calculator-app.js

--- 02-Apuntes/04-calculator-app.js
+++ 02-Apuntes/04-calculator-app.js
@@ -27,18 +27,45 @@
 6 // Components/ __tests__ es la carpeta para hacer los testing
 7 // para resetear todo el proyecto //* ejecutamos el script del archivo reset-project.js
 
-//! ExpoRouter: index y _layout
-// 
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
+//! ExpoRouter: index y _layout (limpiamos proyectos)
+8 // (tabs)borramos la carpeta, y aparece una pantalla que dice que esa pantalla no existe
+9 // borrar el html.tsx y el not_found //* aparece una página "unmatched"
+10 // layout tiene mucha info como el useFonts de RootLayout
+11 // _layout > borramos todo el código y hacemos snipped rnf tab
+12 // cambiamos el nombre del compoennte a RootLayout
+13 // creamos en app/index.tsx (no capitalizado)
+14 // creamos componente defecto dentro de index CalculatorApp //* los componentes tiene que tener el export default 
+15 // en RootLayout añadimos el componente de Expo  //* <Slot />
+16 // <Slot/> le está diciendo a Expo y React Native //* "todo lo que esté por debajo"
+17 // slot le está diciendo "cualquier ruta hija renderidazalo en Slot"
+18 // por lo que RootLayout podemos poner por ejemplo un header / slot /footer
+19 // eliminar carpeta /components en constants/ Colors.ts la dejamos vacía
+20 // eliminar carpeta /hooks 
+21 // en /package.json eliminamos el script "reset-project" 
+
+
+
+//! Fuentes personalizadas y diseño inicial
+22 // empezamo s atrabajar en index nuestra app
+23 // cambiamos el tamaño del h1
+24 // en _layout.tsx a RpptLayout añadimos con
+const [] = useFonts({ //* useFont lo exportamos de "expo-font" 
+  SpaceMono: requestAnimationFrame("../assets/fonts/SpaceMono-Regular.ttf") //* importamos la fuente que tenemos descargada
+})
+25 // ya tenemos la fuente importada a la app, pero para que se renderice, tenemos que hacerlo directamente sobre el componente
+26 // nos vamos a CalculatorApp y en style añadimos el fontFamily :"SpaceMono" //* si no lo reconoce porque está mal escrito pone la fuente por defecto de cada sistema operativo
+27 // al usefonts le ponemos [loaded] //* de esta manera indicamos que cuando se carge la fuentes, entonces podemos renderizar los componentes
+if (!loaded) {
+  return null
+} //* podríamos hacer algún memsaje 
+
+//? el layout solo contiene la configuración del fondo que deberia de aplicarse a todas las páginas
+28 // /contants definimos los colore 04.20 para hacer el proyecto como el profe
+29 // añadimos a colors as const; àra que se lean como colores y no que TS los aplique como strings
+30 // añadimos de RootLayout el view style {backgroundColor:Colors.background}
+31 // añadimos flex 1 para que se estire y ocupe toda la pantalla
+
+
+32 // debajo de Slot ponemos el <StatusBar /> //* lo importamos de Expo no de RN
+33 // style="light"
+34
