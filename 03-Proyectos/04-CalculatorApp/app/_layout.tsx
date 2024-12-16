62e13d6f6047ccb9db7ee2c6a99cd16791b843b4
quintero git <hello.quinteroo@gmail.com>
2024-12-16 23:11:05 +0100

    UPDATE 04-CalculatorApp

---
+++ 03-Proyectos/04-CalculatorApp/app/_layout.tsx
@@ -0,0 +1,29 @@
+import { View } from 'react-native'
+import React from 'react'
+import { Slot } from 'expo-router'
+import {useFonts} from "expo-font"
+import { Colors } from '@/constants/Colors'
+import {StatusBar} from "expo-status-bar"
+
+const RootLayout = () => {
+
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf")
+  })
+
+  if (!loaded) {
+    return null
+  }
+
+
+
+  return (
+    <View style={{backgroundColor:Colors.background,flex:1}}>
+
+      <Slot/>
+      <StatusBar style='light'/>
+    </View>
+  )
+}
+
+export default RootLayout
---
+++ 03-Proyectos/04-CalculatorApp/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CalculatorApp =()=> {
+  return (
+    <View>
+      <Text style={{fontSize:50, fontFamily:"SpaceMono", color: "white"}}>CalculatorApp</Text>
+    </View>
+  )
+}
+
+export default CalculatorApp
