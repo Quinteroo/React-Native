0881c9e541ccffb547a53a51ee2792967cb25373
quintero git <hello.quinteroo@gmail.com>
2024-12-28 17:10:40 +0100

    START 05-navgation-app

---
+++ 02-Apuntes/05-navigation-app.js
@@ -0,0 +1,47 @@
+
+
+//! TIPOS DE NAVEGACIÓN Y ESTILOS
+
+//? Temas puntuales
+// Nativewind
+// StackNavigation
+// Enviar argumentos entre páginas
+// Botones personalizados
+// Estructura de directorios
+// Temas y fuentes personalizadas
+// Colores personalizados
+// Múltiples layouts
+
+//! Inicio de la aplicación
+1 // terminal > iniciamos proyecto > //* npm create expo@latest
+2 // nombre de la app > enter > cambiamos nombre del archivo
+3 // la app que nos viene instalada pro defcto ya tiene mucha cosas instaladas como expo-router
+4 // abrimos emulador de Android
+5 // limpiamos proyecto eliminando > //* contenido /app, /components, /hooks, /constants, /scripts
+6 // eliminamos de package.json el script //* reset
+//? cualquier archivo que pongamos dentro de /app equivale a una pantalla de la aplicaicón
+//? a excepción de algunos archivos con nombres especiales (ej: _layout) que tienen otras funciones predefinidas
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
