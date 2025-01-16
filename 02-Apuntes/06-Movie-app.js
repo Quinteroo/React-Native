5e2e74e2a9afe97adc052aa5500e0998f1fc1e45
quintero git <hello.quinteroo@gmail.com>
2025-01-16 19:13:05 +0100

    CREATE 06-Movie-app

---
+++ 02-Apuntes/06-Movie-app.js
@@ -0,0 +1,40 @@
+
+
+//! MOVIE APP
+// En esta sección vamos a trabajar realizando peticiones HTTP, manejando el
+// caché de las mismas y varios tipos de listas. Puntualmente veremos:
+
+// TanStack Query
+// Axios
+// FlatList
+// Paquetes externos
+// Reutilización de componentes y parametrización de props
+// SafeArea manual
+// Consideraciones de contenido
+// Scroll dentro de otros Scrollable Elements
+// Mapeo y tipado
+
+
+
+//! INCIO DE PROYECTO
+// abre terminal y crea proyecto //* npm create expo@latest
+// nombre del doc //* MovieApp
+// crea el proyecto, como nosotros estamos creando el proyecto dentro de una subcarpeta general,
+// tenemos que eliminar la carpeta git que se creo por defecto, cambiar el nombre a //* 06-Movie-app
+// y hacer el git add . desde la carpeta madre /REACT NATIVE
+
+
+// seguimos el proceso de instalación de NativeWind //? https://www.nativewind.dev/getting-started/react-native
+
+// limpiamos el proyecto con lo que no necesitamos
+// /componentes /tabs /hooks etc
+
+// iniciamos el proyecto y hacemos algún cambio para verificar que estemos conectados
+
+
+
+
+
+
+
+
